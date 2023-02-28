f51b6ad0e086e966ff6eaf121896490a1f3c83a5
Hassan Khalaf <33877200+HassanTheWhale@users.noreply.github.com>
2023-02-28 23:59:27 +0300

    Update DatabaseSeeder.php

--- back-end/database/seeders/DatabaseSeeder.php
+++ back-end/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\eventCategory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +20,16 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
+        // ]);
+
+        // eventCategory::factory()->create([
+        //     'name' => 'Fun Events',
+        //     'picture' => 'https://source.unsplash.com/1920x1080/?FunEvent'
+        // ]);
+
+        // eventCategory::factory()->create([
+        //     'name' => 'Educational Events',
+        //     'picture' => 'https://source.unsplash.com/1920x1080/?Education'
         // ]);
     }
 }
