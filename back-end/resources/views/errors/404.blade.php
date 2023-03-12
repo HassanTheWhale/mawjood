5f1e127fb8fee952bb9c4d4964fdaf71adff55e8
Hassan Khalaf <33877200+HassanTheWhale@users.noreply.github.com>
2023-03-12 22:07:14 +0300

    404 page

---
+++ back-end/resources/views/errors/404.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="top h-8 bg-prime2 text-white text-center overflow-hidden">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-2"></div>
+            <div class="col-8">
+                <h4 class="m-0">Not Found!</h4>
+            </div>
+            <div class="col-2 pe-5">
+            </div>
+        </div>
+    </div>
+    <div class="content h-84">
+
+        <div class="row py-5">
+            <div class="col-md-3 mx-auto text-center">
+                <img src="{{ asset('imgs/404.svg') }}" class="img-fluid" alt="404">
+                <h2 class="mt-5">404</h2>
+                <p class="text-muted">The page you are looking for is not found! <br>
+                    Please <a class="span" href="{{ url('/home') }}">Go to
+                        the home page.</a></p>
+            </div>
+        </div>
+
+    </div>
+    <div class="bottom h-8 bg-prime2 overflow-hidden">
+        <div class="row h-100">
+            <div class="col-0 col-md-3"></div>
+            <div class="col-12 col-md-6 h-100">
+                <div class="row h-100 bg-prime">
+                    <a href="{{ url('/home') }}" class="d-block col-3 h-100 p-0">
+                        <div class="w-100 h-100 d-flex justify-content-center align-items-center">
+                            <img src="{{ asset('imgs/home.png') }}" alt="Home" />
+                        </div>
+                    </a>
+
+                    <a href="{{ url('/search') }}" class="d-block col-3 h-100 p-0">
+                        <div class="w-100 h-100 d-flex justify-content-center align-items-center">
+                            <img src="{{ asset('imgs/search.png') }}" alt="search for user" />
+                        </div>
+                    </a>
+
+                    <a href="{{ url('/certificates') }}" class="d-block col-3 h-100 p-0">
+                        <div class="w-100 h-100 d-flex justify-content-center align-items-center">
+                            <img src="{{ asset('imgs/certificate.png') }}" alt="My certificates" />
+                        </div>
+                    </a>
+
+                    <a href="{{ url('/profile') }}" class="d-block col-3 h-100 p-0">
+                        <div class="w-100 h-100 d-flex justify-content-center align-items-center">
+                            <img src="{{ asset('imgs/profile.png') }}" alt="My Profile" />
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-0 col-md-3"></div>
+        </div>
+    </div>
+    <!-- JavaScript; -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <script src="./js/script.js"></script>
+    </body>
+
+    </html>
+@endsection
