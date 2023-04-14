12eeda5fd2e13fcbb4bc3e3cd562f36c7dae5708
Hassan Khalaf <33877200+HassanTheWhale@users.noreply.github.com>
2023-04-14 17:41:21 +0300

    home

--- back-end/resources/views/admin/layouts/header.blade.php
+++ back-end/resources/views/admin/layouts/header.blade.php
@@ -83,7 +83,7 @@
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
 
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="{{ route('home') }}">
                                     <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Back to website
                                 </a>
