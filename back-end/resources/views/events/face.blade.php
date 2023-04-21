809fc1eb7a718561a663548dd2cfc8c69f551977
Hassan Khalaf <33877200+HassanTheWhale@users.noreply.github.com>
2023-04-22 01:35:16 +0300

    fixing bugs

--- back-end/resources/views/events/face.blade.php
+++ back-end/resources/views/events/face.blade.php
@@ -21,155 +21,140 @@
     <script>
         try {
             // Get access to the device camera
+            const video = document.getElementById('video');
+
             navigator.mediaDevices.getUserMedia({
                     video: true
                 })
                 .then(stream => {
-                    const video = document.getElementById('video');
                     video.srcObject = stream;
                     video.play();
                 })
                 .catch(error => {
+                    Swal.fire(
+                        'Warning',
+                        `Error accessing device camera: ${error.name}`,
+                        'warning'
+                    );
                     console.error(`Error accessing device camera: ${error}`);
                 });
         } catch (error) {
             Swal.fire(
                 'Warning',
-                'Your browser can not open the camera',
-                'warning',
-            )
-
+                'Your browser cannot open the camera',
+                'warning'
+            );
+            console.error(`Error accessing device camera: ${error}`);
         }
 
+
         const captureButton = document.getElementById('capture-button');
-        captureButton.addEventListener('click', () => {
-            let latitude;
-            let longitude;
-            if ("geolocation" in navigator) {
-                if (window.location.protocol === "https:") {
-                    navigator.geolocation.requestAuthorization().then(permissionStatus => {
+        captureButton.addEventListener('click', async () => {
+            try {
+                // Check for geolocation support
+                if ("geolocation" in navigator) {
+                    // Check for secure (HTTPS) connection
+                    if (window.location.protocol === "https:") {
+                        // Request geolocation permission
+                        const permissionStatus = await navigator.geolocation.requestAuthorization();
                         if (permissionStatus === "granted") {
-                            navigator.geolocation.getCurrentPosition(position => {
-                                latitude = position.coords.latitude;
-                                longitude = position.coords.longitude;
-                            }, error => {
-                                Swal.fire({
-                                    icon: 'error',
-                                    title: 'Error',
-                                    text: `Error getting geolocation coordinates: ${error.message}`
-                                });
-                                return; // Return early on error
+                            // Get current position
+                            const position = await new Promise((resolve, reject) => {
+                                navigator.geolocation.getCurrentPosition(resolve, reject);
                             });
+                            const latitude = position.coords.latitude;
+                            const longitude = position.coords.longitude;
+                            // Continue with capturing the image and submitting the form
+                            const video = document.getElementById('video');
+                            const canvas = document.createElement('canvas');
+                            const context = canvas.getContext('2d');
+                            canvas.width = video.videoWidth;
+                            canvas.height = video.videoHeight;
+                            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+                            const image = canvas.toDataURL();
+                            const formData = new FormData(document.getElementById('camera-form'));
+                            formData.append('image', image);
+                            formData.append('latitude', latitude);
+                            formData.append('longitude', longitude);
+                            const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute(
+                                'content');
+                            const response = await fetch(
+                                'https://mawjood.click/faceCheck/{{ $event->id }}/{{ $myuser->id }}/{{ $instance->id }}', {
+                                    method: 'POST',
+                                    body: formData,
+                                    headers: {
+                                        'X-CSRF-TOKEN': csrfToken
+                                    }
+                                });
+                            if (response.ok) {
+                                location.reload();
+                            } else if (response.status === 404) {
+                                Swal.fire(
+                                    'Warning',
+                                    'Your image did not match the one saved',
+                                    'warning',
+                                );
+                            } else if (response.status === 500) {
+                                const data = await response.json();
+                                Swal.fire(
+                                    'Warning',
+                                    'Your image did not match the one saved. Error: ' + data.message,
+                                    'warning'
+                                );
+                            } else {
+                                Swal.fire(
+                                    'Error',
+                                    'Unknown Error',
+                                    'error',
+                                );
+                            }
                         } else {
                             Swal.fire({
                                 icon: 'error',
                                 title: 'Permission Denied',
                                 text: 'Permission denied for geolocation'
                             });
-                            return; // Return early on error
                         }
-                    }).catch(error => {
+                    } else {
                         Swal.fire({
                             icon: 'error',
                             title: 'Error',
-                            text: `Error requesting geolocation permission: ${error.message}`
+                            text: 'Geolocation requires a secure (HTTPS) connection'
                         });
-                        return; // Return early on error
-                    });
+                    }
                 } else {
                     Swal.fire({
                         icon: 'error',
                         title: 'Error',
-                        text: 'Geolocation requires a secure (HTTPS) connection'
+                        text: 'Geolocation not supported in this browser'
                     });
-                    return; // Return early on error
                 }
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Geolocation not supported in this browser'
-                });
-                return; // Return early on error
+            } catch (error) {
+                Swal.fire(
+                    'Error',
+                    'Error capturing image or contacting the server.',
+                    'error',
+                );
+                console.error(error);
             }
-
-
-            // Continue with capturing the image and submitting the form
-            const video = document.getElementById('video');
-            const canvas = document.createElement('canvas');
-            const context = canvas.getContext('2d');
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            context.drawImage(video, 0, 0, canvas.width, canvas.height);
-            const image = canvas.toDataURL();
-
-
-            const formData = new FormData(document.getElementById('camera-form'));
-            formData.append('image', image);
-            formData.append('latitude', latitude);
-            formData.append('longitude', longitude);
-
-            const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-            fetch('https://mawjood.click/faceCheck/{{ $event->id }}/{{ $myuser->id }}/{{ $instance->id }}', {
-                    method: 'POST',
-                    body: formData,
-                    headers: {
-                        'X-CSRF-TOKEN': csrfToken
-                    }
-                })
-                .then(response => {
-                    if (response.status == 200)
-                        location.reload();
-                    else if (response.status == 404)
-                        Swal.fire(
-                            'Warning',
-                            'Your image did not match the one saved',
-                            'warning',
-                        )
-                    else if (response.status == 500)
-                        response.json().then(function(data) {
-                            Swal.fire(
-                                'Warning',
-                                'Your image did not match the one saved. Error: ' + data.message,
-                                'warning'
-                            );
-                        });
-                    else Swal.fire(
-                        'Error',
-                        'Unknown Error',
-                        'error',
-                    )
-                })
-                .catch(error => {
-                    Swal.fire(
-                        'Error',
-                        'Error contacting the server.',
-                        'error',
-                    )
-                    console.error();
-                });
         });
 
+
         const cancel = document.getElementById('cancel-button');
-        cancel.addEventListener('click', () => {
-            // Get the geolocation coordinates
-            let latitude;
-            let longitude;
-            if ("geolocation" in navigator) {
-                if (window.location.protocol === "https:") {
-                    navigator.geolocation.requestAuthorization().then(permissionStatus => {
+        cancel.addEventListener('click', async () => {
+            try {
+                // Get the geolocation coordinates
+                let latitude;
+                let longitude;
+                if ("geolocation" in navigator) {
+                    if (window.location.protocol === "https:") {
+                        const permissionStatus = await navigator.geolocation.requestAuthorization();
                         if (permissionStatus === "granted") {
-                            navigator.geolocation.getCurrentPosition(position => {
-                                latitude = position.coords.latitude;
-                                longitude = position.coords.longitude;
-                            }, error => {
-                                Swal.fire({
-                                    icon: 'error',
-                                    title: 'Error',
-                                    text: `Error getting geolocation coordinates: ${error.message}`
-                                });
-                                return; // Return early on error
+                            const position = await new Promise((resolve, reject) => {
+                                navigator.geolocation.getCurrentPosition(resolve, reject);
                             });
+                            latitude = position.coords.latitude;
+                            longitude = position.coords.longitude;
                         } else {
                             Swal.fire({
                                 icon: 'error',
@@ -178,70 +163,63 @@
                             });
                             return; // Return early on error
                         }
-                    }).catch(error => {
+                    } else {
                         Swal.fire({
                             icon: 'error',
                             title: 'Error',
-                            text: `Error requesting geolocation permission: ${error.message}`
+                            text: 'Geolocation requires a secure (HTTPS) connection'
                         });
                         return; // Return early on error
-                    });
+                    }
                 } else {
                     Swal.fire({
                         icon: 'error',
                         title: 'Error',
-                        text: 'Geolocation requires a secure (HTTPS) connection'
+                        text: 'Geolocation not supported in this browser'
                     });
                     return; // Return early on error
                 }
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Geolocation not supported in this browser'
-                });
-                return; // Return early on error
-            }
 
-            const formData = new FormData(document.getElementById('camera-form'));
-            formData.append('cancel', '1');
-            formData.append('latitude', latitude);
-            formData.append('longitude', longitude);
+                const formData = new FormData(document.getElementById('camera-form'));
+                formData.append('cancel', '1');
+                formData.append('latitude', latitude);
+                formData.append('longitude', longitude);
 
-            const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-            fetch('https://mawjood.click/faceCheck/{{ $event->id }}/{{ $myuser->id }}/{{ $instance->id }}', {
-                    method: 'POST',
-                    body: formData,
-                    headers: {
-                        'X-CSRF-TOKEN': csrfToken
-                    }
-                })
-                .then(response => {
-                    if (response.status == 200)
-                        location.reload();
-                    else if (response.status == 404) {
-                        response.json().then(function(data) {
-                            Swal.fire(
-                                'Warning',
-                                'Your image did not match the one saved. Error: ',
-                                'warning'
-                            );
-                        });
-                    } else if (response.status == 500)
-                        Swal.fire(
-                            'Error',
-                            'Your image could not be captured, please try again',
-                            'error',
-                        )
-                    else Swal.fire(
+                const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+                const response = await fetch(
+                    'https://mawjood.click/faceCheck/{{ $event->id }}/{{ $myuser->id }}/{{ $instance->id }}', {
+                        method: 'POST',
+                        body: formData,
+                        headers: {
+                            'X-CSRF-TOKEN': csrfToken
+                        }
+                    });
+
+                if (response.status == 200) {
+                    location.reload();
+                } else if (response.status == 404) {
+                    const data = await response.json();
+                    Swal.fire(
+                        'Warning',
+                        'Your image did not match the one saved. Error: ',
+                        'warning'
+                    );
+                } else if (response.status == 500) {
+                    Swal.fire(
+                        'Error',
+                        'Your image could not be captured, please try again',
+                        'error',
+                    );
+                } else {
+                    Swal.fire(
                         'Error',
                         'Unknown Error',
                         'error',
-                    )
-                })
-                .catch(error => {
-                    console.error(`Error submitting form: ${error}`);
-                });
+                    );
+                }
+            } catch (error) {
+                console.error(`Error submitting form: ${error}`);
+            }
         });
     </script>
 @endsection
