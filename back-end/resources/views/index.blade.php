0be9c2019b4991349f1c02df6169c80893945155
Hassan Khalaf <33877200+HassanTheWhale@users.noreply.github.com>
2023-05-01 08:39:36 +0300

    design fix

--- back-end/resources/views/index.blade.php
+++ back-end/resources/views/index.blade.php
@@ -1,28 +1,20 @@
 @extends('layouts.master')
 @section('content')
-<div class="landing">
-<div
-      class="landing-top d-flex justify-content-center align-items-center h-70"
-    >
-      <img src="./imgs//logo.png" width="300px" alt="Mawjood Logo" />
-    </div>
-    <div class="landing-bottom h-30 mx-auto">
-      <div
-        class="landing-downlay bg-primary h-100 d-flex justify-content-center align-items-end"
-      >
-        <div class="landing-downlay bg-white h-80 w-100 p-3 overflow-auto">
-          <h4 class="text-center mb-3">Welcome</h4>
-          <a
-            href="./login"
-            class="btn btn-primary text-white w-100 mb-3 py-2"
-            >Login to your account</a
-          >
-          <p class="mb-0 text-muted text-center">
-            Don't have an account?
-            <a href="./register" class="span">Register Here</a>
-          </p>
+    <div class="landing">
+        <div class="landing-top d-flex justify-content-center align-items-center h-60">
+            <img src="./imgs//logo.png" width="300px" alt="Mawjood Logo" />
         </div>
-      </div>
+        <div class="landing-bottom h-50 mx-auto">
+            <div class="landing-downlay bg-primary h-100 d-flex justify-content-center align-items-end">
+                <div class="landing-downlay bg-white h-90 w-100 p-5 overflow-auto">
+                    <h4 class="text-center mb-3">Welcome</h4>
+                    <a href="./login" class="btn btn-primary text-white w-100 mb-3 py-2">Login to your account</a>
+                    <p class="mb-0 text-muted text-center">
+                        Don't have an account?
+                        <a href="./register" class="span">Register Here</a>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 @endsection
