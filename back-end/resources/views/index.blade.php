26499f35b7c7b82df4f1e05994d994e506f84e91
Hassan Khalaf <33877200+HassanTheWhale@users.noreply.github.com>
2023-05-19 14:11:57 +0300

    fixing landing page

--- back-end/resources/views/index.blade.php
+++ back-end/resources/views/index.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.master')
 @section('content')
-    <div class="landing">
-        <div class="landing-top d-flex justify-content-center align-items-center h-60">
-            <img src="./imgs//logo.png" width="300px" alt="Mawjood Logo" />
-        </div>
+    <div class="landing d-flex justify-content-center align-items-center">
         <div class="landing-bottom h-50 mx-auto">
-            <div class="landing-downlay bg-primary h-100 d-flex justify-content-center align-items-end">
-                <div class="landing-downlay bg-white h-90 w-100 p-5 overflow-auto">
+            <div class="landing-downlay bg-primary d-flex pt-4 justify-content-center align-items-end">
+                <div
+                    class="landing-downlay bg-white w-100 h-90 px-3 pb-4 overflow-auto d-flex flex-column justify-content-center align-items-center">
+                    <div class="text-center">
+                        <img src="./imgs/logo.png" width="150px" alt="Mawjood Logo" />
+                    </div>
+
                     <h4 class="text-center mb-3">Welcome</h4>
                     <a href="./login" class="btn btn-primary text-white w-100 mb-3 py-2">Login to your account</a>
                     <p class="mb-0 text-muted text-center">
